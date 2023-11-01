c246ccfe24dad392609fbc8f89ba0f004786c174
alex13slem <alexshherba@gmail.com>
2023-11-02 00:03:34 +0300

    add Socials.admin

--- src/schemas/socialsSchema.ts
+++ src/schemas/socialsSchema.ts
@@ -1,7 +1,6 @@
 import { z } from "astro/zod";
 
 export const socialsSchema = z.object({
-  name: z.string(),
+  title: z.string(),
   link: z.string().url(),
-  fontFamily: z.string().toLowerCase().optional(),
 });
