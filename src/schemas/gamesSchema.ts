7e2af1c4ca06e72ca2637b90f58d2cd62932c55e
alex13slem <alexshherba@gmail.com>
2023-11-16 23:47:43 +0300

    asd

--- src/schemas/gamesSchema.ts
+++ src/schemas/gamesSchema.ts
@@ -2,12 +2,6 @@ import { z } from "astro/zod";
 
 export const gamesSchema = z.object({
   title: z.string(),
-  genre: z.enum([]),
-  developer: z.string(),
-  publisher: z.string(),
-  date: z.date(),
-  description: z.string(),
-  body: z.string(),
   thumbnail: z.string(),
   tags: z.array(z.string().optional()),
   link: z.string().url().optional(),
@@ -20,3 +14,24 @@ export const gamesSchema = z.object({
     .optional(),
   category: z.enum(["test", "dev", "hot", "new", ""]).optional(),
 });
+
+// export const gamesSchema = z.object({
+//   title: z.string(),
+//   genre: z.enum([]),
+//   developer: z.string(),
+//   publisher: z.string(),
+//   date: z.date(),
+//   description: z.string(),
+//   body: z.string(),
+//   thumbnail: z.string(),
+//   tags: z.array(z.string().optional()),
+//   link: z.string().url().optional(),
+//   links: z
+//     .object({
+//       playgame: z.string().url().optional(),
+//       apple: z.string().url().optional(),
+//       google: z.string().url().optional(),
+//     })
+//     .optional(),
+//   category: z.enum(["test", "dev", "hot", "new", ""]).optional(),
+// });
