e18b67c286b152b136bd761f4168d2b6d76fbd26
alex13slem <alexshherba@gmail.com>
2023-11-17 00:17:52 +0300

    asd

--- src/schemas/gamesSchema.ts
+++ src/schemas/gamesSchema.ts
@@ -1,28 +1,7 @@
 import { z } from "astro/zod";
 
-export const gamesSchema = z.object({
-  title: z.string(),
-  thumbnail: z.string(),
-  tags: z.array(z.string().optional()),
-  link: z.string().url().optional(),
-  links: z
-    .object({
-      playgame: z.string().url().optional(),
-      apple: z.string().url().optional(),
-      google: z.string().url().optional(),
-    })
-    .optional(),
-  category: z.enum(["test", "dev", "hot", "new", ""]).optional(),
-});
-
 // export const gamesSchema = z.object({
 //   title: z.string(),
-//   genre: z.enum([]),
-//   developer: z.string(),
-//   publisher: z.string(),
-//   date: z.date(),
-//   description: z.string(),
-//   body: z.string(),
 //   thumbnail: z.string(),
 //   tags: z.array(z.string().optional()),
 //   link: z.string().url().optional(),
@@ -35,3 +14,32 @@ export const gamesSchema = z.object({
 //     .optional(),
 //   category: z.enum(["test", "dev", "hot", "new", ""]).optional(),
 // });
+
+const zLang = z.object({ voice: z.boolean(), text: z.boolean() });
+
+export const gamesSchema = z.object({
+  title: z.string(),
+  genre: z.enum([
+    "arcade",
+    "rpg",
+    "shooter",
+    "racing",
+    "action",
+    "strategy",
+    "fighting",
+    "horror",
+    "simulator",
+    "stealth",
+  ]),
+  developer: z.string(),
+  publisher: z.string(),
+  date: z.date(),
+  description: z.string(),
+  body: z.string(),
+  langs: z.object({ ru: zLang, en: zLang }),
+  thumbnail: z.string(),
+  hero_image: z.string(),
+  ad_banner: z.object({ src: z.string(), alt: z.string() }),
+
+  // category: z.enum(["test", "dev", "hot", "new", ""]).optional(),
+});
